f8396d4384f68e7cf81d87f40d82b36d061fa49c
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-01-02 21:25:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     #rackup app.rb to run
     #ctl c to stop running the server
     #copy paste the ip address
-    "Started my server using Shotgun!"
+    "Started my server using shotgun!"
     #it takes a while to load shotgun
   end
 
