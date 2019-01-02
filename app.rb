a9c1c5d800f66166c18337c6f32eb40fadd90cf7
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-01-02 21:08:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
+    #rackup app.rb to run
+    #ctl c to stop running the server
+    #copy paste the ip address
     "Welcome to your app!!!! I BUILT THIS! "
+    #it takes a while to load shotgun
   end
 
 end
